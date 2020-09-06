ead6ffdb27dfda722d93943a3b09af3ab031ee2b
Fazal Mahmud Niloy <niloooy@gmail.com>
2020-09-06 23:37:23 +0600

    [:o:] # comments deleted

--- src/StopwatchCPU.java
+++ src/StopwatchCPU.java
@@ -52,7 +52,7 @@ public class StopwatchCPU {
      * @param args the command-line arguments
      */
     public static void main(String[] args) throws FileNotFoundException {
-        // int n = Integer.parseInt("10000000");
+
         Scanner scanner = new Scanner(new File("src/4K_ints.txt"));
         int [] tall = new int [4000];
         int index = 0;
@@ -74,24 +74,5 @@ public class StopwatchCPU {
 
         double time = timer.elapsedTime();
         StdOut.printf(" (%.2f seconds)\n", time);
-
-
-        // sum of square roots of integers from 1 to n using Math.sqrt(x).
-        /*StopwatchCPU timer1 = new StopwatchCPU();
-        double sum1 = 0.0;
-        *//*for (int i = 1; i <= n; i++) {
-            sum1 += Math.sqrt(i);
-        }
-        double time1 = timer1.elapsedTime();
-        StdOut.printf("%e (%.2f seconds)\n", sum1, time1);*//*
-
-        // sum of square roots of integers from 1 to n using Math.pow(x, 0.5).
-        StopwatchCPU timer2 = new StopwatchCPU();
-        double sum2 = 0.0;
-        for (int i = 1; i <= n; i++) {
-            sum2 += Math.pow(i, 0.5);
-        }
-        double time2 = timer2.elapsedTime();
-        StdOut.printf("%e (%.2f seconds)\n", sum2, time2);*/
     }
 }
